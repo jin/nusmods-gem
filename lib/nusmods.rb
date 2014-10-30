77591a7956c2528fe634503cc438bda7574877e1
jin <jin@crypt.sg>
2014-10-31 01:58:03 +0800

    Removed unused code

--- lib/nusmods.rb
+++ lib/nusmods.rb
@@ -2,15 +2,4 @@ require 'faraday'
 require 'faraday_middleware'
 require 'nusmods/client'
 
-directory = File.expand_path(File.dirname(__FILE__))
-
-module NUSMods 
-  class << self
-
-    ## configure default
-    def configure
-      yield self
-    end
-
-  end
-end
+File.expand_path(File.dirname(__FILE__))
